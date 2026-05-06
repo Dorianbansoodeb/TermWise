7e7dfe766eb222408085c11cf6bf5adc555ce55e
Dorianbansoodeb <dorian.akash@gmail.com>
2026-05-05 23:09:53 -0400

    Initial Commit

---
+++ TermWise/TermWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TermWiseApp.swift
+//  TermWise
+//
+//  Created by Dorian Bansoodeb on 2026-05-05.
+//
+
+import SwiftUI
+
+@main
+struct TermWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TermWiseTests/TermWiseTests.swift
@@ -0,0 +1,18 @@
+//
+//  TermWiseTests.swift
+//  TermWiseTests
+//
+//  Created by Dorian Bansoodeb on 2026-05-05.
+//
+
+import Testing
+
+struct TermWiseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
